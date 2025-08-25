7fa89a1be52469ca1af138787922b5365a06d512
SAI ARJUN <112474122+SAI-ARJUN@users.noreply.github.com>
2025-08-25 09:14:30 +0530

    Add files via upload

---
+++ Conditionals-Loops/src/Loops.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        /*
+        Syntax of for loop:
+        for(initailisation; condition; increment/decrement){
+            //body
+        }
+         */
+
+        //Print numbers from 1 to 5
+
+//        for(int n = 1; n<=5;n++){
+//            System.out.print(n);
+//        }
+
+        int n = in.nextInt();
+
+//        for (int i = 1; i <= n ; i++) {
+//            System.out.print(i+" ");
+//
+//        }
+
+        int num = 1;
+        while(num<=n){
+            System.out.print(num+" ");
+            num++;
+        }
+//        do{
+//            System.out.print("Executes atleast once if the condition is also false");
+//        }while(num!=1);
+    }
+}
