1171816ece551a0a60f3aab7cd83fcb4f0d34c3f
SAI ARJUN <112474122+SAI-ARJUN@users.noreply.github.com>
2025-08-26 12:25:02 +0530

    Update Area_of_shapes.java

--- Switch/src/Area_of_shapes.java
+++ Switch/src/Area_of_shapes.java
@@ -5,13 +5,30 @@ public class Area_of_shapes {
         Scanner in = new Scanner(System.in);
 
         int n = in.nextInt();
-        int area;
+        double area;
         switch (n){
             case 1:
                 int r = in.nextInt();
                 area = Math.PI*r*r;
-                System.out.println(area);
+                System.out.println("Area of Circle is : "+area);
+                break;
+            case 2:
+                int s = in.nextInt();
+                area = s*s;
+                System.out.println("Area of Square is : "+area);
+                break;
 
+            case 3:
+                int l = in.nextInt();
+                int b = in.nextInt();
+                area = l*b;
+                System.out.println("Area of Rectangle is: "+ area);
+                break;
+
+            default:
+                System.out.println("Enter valid choice...");
+                break;
         }
+
     }
 }
