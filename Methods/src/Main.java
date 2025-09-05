cdd5073dfc1087aed3e1c3aaa4b980b5b690bb3b
SAI ARJUN <112474122+SAI-ARJUN@users.noreply.github.com>
2025-09-05 13:38:02 +0530

    Add files via upload

---
+++ Methods/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num1,num2,sum;
+        System.out.print("Enter 1st number: ");
+        num1 = in.nextInt();
+
+        System.out.print("Enter 2nd number: ");
+        num2 = in.nextInt();
+
+        sum = num1+num2;
+
+        System.out.println("Sum = "+sum);
+
+
+    }
+}
