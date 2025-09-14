cf399c976b339a3d05946989176567f95a120bc7
SAI ARJUN <112474122+SAI-ARJUN@users.noreply.github.com>
2025-09-14 20:10:01 +0530

    Add files via upload

---
+++ HackerRank/src/StairCase.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class StairCase {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<n-i;j++){
+                System.out.print(" ");
+            }
+
+            for(int k=0;k<i;k++){
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
+}
